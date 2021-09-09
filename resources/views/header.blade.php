5cb6eab15097ab34fa6663531b9654c170d67c1b
Your name here <alaghbandrad_ali@yahoo.com>
2021-09-09 14:10:24 -0400

    logout and login done

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -17,7 +17,7 @@ if(Session::has('user'))
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="/">E-Comm</a>
+        <a class="navbar-brand" href="/">E-Commerce Website</a>
       </div>
   
       <!-- Collect the nav links, forms, and other content for toggling -->
